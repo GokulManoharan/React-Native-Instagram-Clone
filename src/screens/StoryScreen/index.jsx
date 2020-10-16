e2b84c9c589e9acecc7fa7bf025e6ca92a21d920
Gokul Manoharan <gokool2u@gmail.com>
2020-10-16 22:34:28 +0530

    user stories component in progress

--- src/screens/StoryScreen/index.jsx
+++ src/screens/StoryScreen/index.jsx
@@ -1,17 +1,46 @@
-import React from 'react';
-import { View, Text, SafeAreaView, Image } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { SafeAreaView, ImageBackground, ActivityIndicator, TouchableWithoutFeedback } from 'react-native';
 import { useRoute } from "@react-navigation/native";
 import storiesData from "../../data/stories";
+import styles from "./styles"
 
 const StoryScreen = _ => {
-
     const route = useRoute();
-    const { userId } = route.params;
-    const userStories = storiesData.find(story => story.user.id === userId).stories;
+
+    const [userStories, setUserStories] = useState([]);
+    const [activeStoryIndex, setActiveStoryIndex] = useState(0);
+    const [activeStory, setActiveStory] = useState(null);
+
+    useEffect(_ => {
+        const { userId } = route.params;
+        const userStories = storiesData.find(story => story.user.id === userId).stories;
+        setUserStories(userStories);
+        setActiveStoryIndex(0)
+    }, [])
+
+    useEffect(_ => {
+        if (userStories.length > 0)
+            setActiveStory(userStories[activeStoryIndex]);
+    }, [activeStoryIndex, userStories])
+
+    const handleOnPress = e => {
+        console.log(e.nativeEvent.screenX)
+    }
+
+    if (!activeStory) {
+        return (
+            <SafeAreaView>
+                <ActivityIndicator />
+            </SafeAreaView>
+        )
+    }
+
 
     return (
-        <SafeAreaView>
-            <Text>Story goes here</Text>
+        <SafeAreaView style={styles.container}>
+            <TouchableWithoutFeedback onPress={handleOnPress}>
+                <ImageBackground source={activeStory.source} style={styles.image} />
+            </TouchableWithoutFeedback>
         </SafeAreaView>
     )
 }
